8f1afc223a11ac32d820e2ce6367c372670f9df6
Pathum Senanyake <pathum.2019557@iit.ac.lk>
2024-11-04 23:49:19 +0530

    redirects to /calculator by default

---
+++ src/routes/calculator/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	const names: string = 'Pinkz';
+</script>
+
+<h1>Welcome to {names}</h1>
+<p>
+	Visit <a href="https://svelte.dev/docs/kit"> svelte.dev/docs/kit </a> to read the documentation.
+</p>
